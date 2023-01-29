c13211d63343fae39c970dd5aaeef330e5a14149
katja <ekaterina.kozlova@canter.fi>
2023-01-29 21:00:33 +0200

    Work on project. Stage 2/5: Sold!

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,17 +1,23 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        System.out.println("""
-                Cinema:
-                  1 2 3 4 5 6 7 8
-                1 S S S S S S S S
-                2 S S S S S S S S
-                3 S S S S S S S S
-                4 S S S S S S S S
-                5 S S S S S S S S
-                6 S S S S S S S S
-                7 S S S S S S S S""");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = in.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int cols = in.nextInt();
+
+        int numOfSeats = rows * cols;
+        System.out.println("Total income:");
+        if (numOfSeats <= 60) {
+            System.out.println("$" + numOfSeats * 10);
+        } else {
+            int front = rows / 2;
+            System.out.println("$" + (front * cols * 10 + (rows - front) * cols * 8));
+        }
     }
 }
